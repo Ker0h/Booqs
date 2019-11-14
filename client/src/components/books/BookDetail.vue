c8ee544ea9f177a25de121677cf9d74bf12e2111
Yannick Willems <ywillems96@gmail.com>
2019-11-14 14:37:11 +0100

    BookDetail WIP

---
+++ client/src/components/books/BookDetail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="book-detail">
+        <b-card :title="book.title">
+            <b-card-text>
+                <p>{{book.description}}</p>
+                <ul>
+                    <li><b>Genre</b>: {{book.genre}}</li>
+                </ul>
+            </b-card-text>
+        </b-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BookDetail"
+}
+</script>
+
