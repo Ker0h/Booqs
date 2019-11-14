fb464e0fbd12e6482ab4a7383c97e4c8f7f23aef
Yannick Willems <ywillems96@gmail.com>
2019-11-14 10:51:25 +0100

    About WIP

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item href="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-container>
+      <router-view />
+    </b-container>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
