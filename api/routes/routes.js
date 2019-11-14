fee8c49aeb8ed2eeeabe48d22695607e3fe6cb4f
Yannick Willems <ywillems96@gmail.com>
2019-11-14 09:37:49 +0100

    Fix

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,10 +1,10 @@
 const routes = require('express').Router()
-const books_route = require('./books.route')
+const books_routes = require('./books.routes')
 
 const NotFoundResponse = require('../models/responses/notFound.response')
 
-routes.get('/', (req, res) => res.status(200).json({"messsage": "Hello World!"}))
-routes.use('/books', books_route)
+routes.get('/', (req, res) => res.status(200).json({"message": "Hello World!"}))
+routes.use('/books', books_routes)
 routes.use('*', (req, res) => new NotFoundResponse(res))
 
 module.exports = routes
